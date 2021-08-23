fbe04635ed953e740f23e57cc27244c7f1e75b17
claudiocassiano <claudiopecassiano@gmail.com>
2021-08-23 11:20:18 -0300

    Acrescentado a função de multiplicação.
    Somente acrescentando função mult.

--- PythonExercicio/Exercicio.py
+++ PythonExercicio/Exercicio.py
@@ -4,11 +4,15 @@ def soma_dois_num():
     soma = n1 + n2
     print(f'A soma do número {n1} e do número {n2} é: {soma}')
 
+def mult_dois_num():
+    mult = n1 * n2
+    print('A multiplicação do número {} e o número {} são: {}'.format(n1, n2, mult))
+
 n1 = int(input('Digite um número: '))
 n2 = int(input('Digite outro número: '))
 
 print(soma_dois_num())
-
+print(mult_dois_num())
 
 nome = input("Diga seu nome: ")
 print('Processando...............')
