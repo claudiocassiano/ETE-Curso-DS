11e975bd9929ba67a11ef7589df7f9be4c06b4ff
claudiocassiano <claudiopecassiano@gmail.com>
2021-08-22 21:25:40 -0300

    Dois jeitos de saída.
    Dois formatos de saída de dados.

--- Python/Exercicio.py
+++ Python/Exercicio.py
@@ -3,5 +3,6 @@ from time import sleep
 nome = input("Diga seu nome: ")
 print('Processando.....')
 sleep(2)
-print("Seu nome é : {}".format(nome))
+print("Seu nome é: {}".format(nome))
+print(f'O seu nome é: {nome}')
 
