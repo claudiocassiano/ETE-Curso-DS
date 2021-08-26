f0cf17b5e013afbcaa268392bcc4cdd8f094d5fd
claudiocassiano <claudiopecassiano@gmail.com>
2021-08-26 15:11:15 -0300

    Mais exercicios atualizados
    Atualizados 26/08/2021

--- site-exemplo/exercicioDIO5.py
+++ site-exemplo/exercicioDIO5.py
@@ -8,6 +8,7 @@ while a <= 10:
 nota1 = int(input('Digite uma nota de 0 a 10: '))
 while nota1 > 10:
     nota1 = int(input('Nota inválida. Entre com a nota correta: '))
+    
 nota2 = int(input('Digite uma nota de 0 a 10: '))
 while nota2 > 10:
     nota2 = int(input('Nota inválida. Entre com a nota correta: '))
---
+++ site-exemplo/exercicioDIO6.py
@@ -0,0 +1,40 @@
+
+lista = [12,10,7,5]
+lista_animal = ['cachorro','gato','elefante','lobo','arara']
+"""
+soma = 0
+for x in lista:
+  #  print(x)
+    soma += x
+print(soma)
+"""
+lista_animal[0] = 'macaco' #tupla () vc não consegue mecher, lista[]
+print(lista_animal)
+tupla = (1,10,12,14)
+print(tupla)
+
+#sum soma, min valor minimo, max valor maximo
+#print(lista_animal)
+#print(sum(lista))
+#print(min(lista_animal))
+#palavra = str(input('Digite um animão para ver se está na lista: '))
+#if palavra in lista_animal:
+#    print('O animal {} já existe na lista!'.format(palavra))
+#else:
+#    print('O animal {} não existe na lista!'.format(palavra))
+
+#lista_animal.append('lobo')
+#print(lista_animal)
+#lista_animal.pop() #pop retira o ultimmo da lista, no caso lobo
+#lista_animal.pop(1) #com a posição retira da posição
+#print(lista_animal)
+#lista_animal.remove('elefante')
+#print(lista_animal)
+#lista.sort() #ordena a lista
+#lista_animal.sort()
+#print(lista)
+#print(lista_animal)
+#lista.reverse() #reverte
+#print(lista)
+
+
