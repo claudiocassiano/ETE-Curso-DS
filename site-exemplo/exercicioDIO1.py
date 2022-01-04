a204ba85b6f3afc3347720e7423e91c84b6f2c4d
claudiocassiano <claudiopecassiano@gmail.com>
2022-01-03 22:22:08 -0300

    Maior ou menor
    acrescentando um print formatado

--- site-exemplo/exercicioDIO1.py
+++ site-exemplo/exercicioDIO1.py
@@ -6,7 +6,7 @@ c = int(input('Digite mais um número: '))
 if a > b and a > c:
     print('O número {} é maior que o número {} e {}.'.format(a, b, c))
 elif b > a and b > c:
-    print('O número {} é maior  que o número {} e {}.'.format(b, a, c))
+    print('O número {} é maior que o número {} e {}.'.format(b, a, c))
 else:
     print('O número {} é maio que o número {} e {}'.format(c, b, a))
 print('Fim do programa.')
